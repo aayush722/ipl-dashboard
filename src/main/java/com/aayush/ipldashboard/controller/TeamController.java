4a413d342434d57887b82588b0ae17b14bdf31d8
aayush722 <arora.aayush97@gmail.com>
2022-03-31 12:20:46 +0530

    add match api

--- src/main/java/com/aayush/ipldashboard/controller/TeamController.java
+++ src/main/java/com/aayush/ipldashboard/controller/TeamController.java
@@ -6,11 +6,9 @@ import com.aayush.ipldashboard.repository.MatchRepository;
 import com.aayush.ipldashboard.repository.TeamRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -30,5 +28,12 @@ public class TeamController {
         Team team = teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchByYear(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return matchRepository.getMatchByTeamBetweenDate(teamName, startDate, endDate);
     }
 }
--- src/main/java/com/aayush/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/aayush/ipldashboard/repository/MatchRepository.java
@@ -3,13 +3,23 @@ package com.aayush.ipldashboard.repository;
 import com.aayush.ipldashboard.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable page);
+
+    @Query("Select m from Match m where (m.team1 = :team or m.team2 = :team) and date Between :startDate and :endDate order by date desc")
+    List<Match> getMatchByTeamBetweenDate(
+            @Param("team") String team,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 
     default List<Match> findLatestMatchesByTeam(String teamName, int count) {
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
